910767d3ef19e2a1a25ee1ae7575f0a1750dc5de
Jerry <exnxs@hotmail.com>
2017-08-06 15:40:40 -0400

    Add style to first page section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
+    mixins = require('postcss-mixins'),
     autoprefixer = require('autoprefixer'),
     simpleVar = require('postcss-simple-vars'),
     nestedCss = require('postcss-nested'),
@@ -7,6 +8,10 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, simpleVar, nestedCss, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, simpleVar, nestedCss, autoprefixer]))
+    .on('error', function(error){
+      console.log(error.message);
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
